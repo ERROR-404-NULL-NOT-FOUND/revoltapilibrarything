caca012cbb60d22d2438b3a09b26913da3c552ba
ERROR <giggabyte6@gmail.com>
2022-05-01 16:16:22 -0500

    Added more data on user fetch

--- user.py
+++ user.py
@@ -8,6 +8,12 @@ class User:
 
     username:str #:User's name
     status:str #:User's status
+    online: bool #:Whether or not the user is online
+
+    profile: dict #:User's profile
+    badges: int #:Bitfield of user's badges
+    flags: int #:Enum of user's flags
+
     bot:bool #:Whether or not the user is a bot
     owner:str #:If user is a bot, the ID of the owner
 
@@ -25,12 +31,15 @@ class User:
             self._session='x-session-token'
         response=requests.get(f'https://api.revolt.chat/users/{self.id}',headers={self._session:self._token})
         data=processresponse.processresponse(response)
-        self._token=token
         self.id=data["_id"]
         self.username=data["username"]
+        self.bot=data["bot"]
+        if(self.bot): self.owner=data["owner"]
+        if("profile" in data): self.profile=data["profile"]
+        if("badges" in data): self.badges=data["badges"]
+        if("flags" in data): self.flags=data["flags"]
         if("status" in data): self.status=data["status"]
         if("avatar" in data): self.avatar=data["avatar"]
-        if("relations" in data and "relationship" in data):
-          self.relations=data["relations"]
-          self.relationship=data["relationship"]
+        if("relations" in data): self.relations=data["relations"]
+        if("relationship" in data): self.relationship=data["relationship"]
     #Todo: fetch user bio
